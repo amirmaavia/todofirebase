d02540a658e444903f81d9f4a7bdb49689454083
amirmaavia <Maaviyamansoori222@gmail.com>
2020-08-16 01:14:44 +0500

    create element

--- style.js
+++ style.js
@@ -1,47 +1,68 @@
-function Addfuction(e){
+function Addfuction(e) {
     e.preventDefault();
     console.log("added new todo")
-    const todo = document.querySelector('input').value  
+    const todo = document.querySelector('input').value
     console.log(todo)
 
     //add
     var database = firebase.database();
 
 
-UseRef = database.ref().child("users")
-function writeuserdata(userId, todo, ){
-UseRef.child(userId).set({
-    user: todo,
-   
-})
-}
+    UseRef = database.ref().child("users")
+    function writeuserdata(userId, todo, ) {
+        UseRef.child(userId).set({
+            user: todo,
 
-setTimeout(function(){
-    console.log("going to user ")
-    writeuserdata(Date.now(),todo)
-},1500)
-
-
-
-//get
-
-    var task_array = [];
-    UseRef.on('value', function(snapshot){
-        // console.log("get todos", snapshot.val())
-        snapshot.forEach(function(childsnapshot){
-            var childValue = childsnapshot.val()
-            task_array.push(Object.values(childValue));
-        for(var i = 0; i< task_array.length; i++){
-           console.log(task_array[i]) 
-           
-        }
         })
-})
+    }
+
+    setTimeout(function () {
+        console.log("going to user ")
+        writeuserdata(Date.now(), todo)
+    }, 1500)
+
+
+
+    //get
+    var task_array = [];
+
+    UseRef.on('value', function (snapshot) {
+       
+        // console.log("get todos", snapshot.val())
+        snapshot.forEach(function (snapshot) {
+            // var childKey = snapshot.key;
+            // var childValue = snapshot.val()
+            // task_array.push(Object.values(childValue));
+            for (var i = 0; i < task_array.length; i++) {
+                console.log(task_array[i])
+                
+                 }
+        })
+        
+        
+         var heading =  document.createElement("h1")
+         heading.innerHTML = (task_array)
+        document.body.appendChild(heading);
+
+
+       var btn = document.createElement("button")
+       btn.innerHTML = "Delte"
+       document.body.appendChild(btn) 
+       
+    }
+
+  
+   
+        
+        
+    
+
+    )}
 
 const form = document.querySelector(".form")
 
-form.addEventListener("submit", Addfuction)
-}
+form.addEventListener("click", Addfuction)
+
 
 
 
